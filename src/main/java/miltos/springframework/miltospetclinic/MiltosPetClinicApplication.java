d6ac807211251fc0c5036ff2c0ed0b1ecbaa0755
MiltosDim <miltosdimitriadisceid@gmail.com>
2021-04-03 22:38:53 +0300

    Initial commit

---
+++ src/main/java/miltos/springframework/miltospetclinic/MiltosPetClinicApplication.java
@@ -0,0 +1,13 @@
+package miltos.springframework.miltospetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiltosPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiltosPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/miltos/springframework/miltospetclinic/MiltosPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package miltos.springframework.miltospetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiltosPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
