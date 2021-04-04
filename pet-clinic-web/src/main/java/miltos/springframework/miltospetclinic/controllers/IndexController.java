2f719358bc5e63c7a3e651ed26bc8063b54e1734
MiltosDim <miltosdimitriadisceid@gmail.com>
2021-04-04 22:48:42 +0300

    Controller and Index Page (Thymeleaf)

---
+++ pet-clinic-web/src/main/java/miltos/springframework/miltospetclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package miltos.springframework.miltospetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
