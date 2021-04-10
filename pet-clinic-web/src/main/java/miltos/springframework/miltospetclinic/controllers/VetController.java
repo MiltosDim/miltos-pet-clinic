55e9a148f2e12622109afc094122de31fc702655
MiltosDim <miltosdimitriadisceid@gmail.com>
2021-04-10 19:45:44 +0300

    Vets Page List

--- pet-clinic-web/src/main/java/miltos/springframework/miltospetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/miltos/springframework/miltospetclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package miltos.springframework.miltospetclinic.controllers;
 
+import miltos.springframework.miltospetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
